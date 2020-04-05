6ccbaed9cc5969c21b828be51f5d95168c27de18
ShizukuIchi <shizukuichi@gmail.com>
2020-04-05 17:31:24 +0800

    ci:  esm

--- src/index.test.js
+++ src/index.test.js
@@ -1,8 +1,7 @@
-const generateIdGenerator = require('./index');
+const generateIdGenerator = require("./index");
 
-describe('test id-generator', () => {
-  it('generate id from 5', () => {
-    const mock = jest.fn();
+describe("test id-generator", () => {
+  it("generate id from 5", () => {
     const generateFrom5 = generateIdGenerator(5);
     const first = generateFrom5();
     const second = generateFrom5();
@@ -10,7 +9,7 @@ describe('test id-generator', () => {
     expect(first).toBe(5);
     expect(second).toBe(6);
   });
-  it('run 1 thousand times and get a result of 999', () => {
+  it("run 1 thousand times and get a result of 999", () => {
     const generateId = generateIdGenerator();
     for (let i = 999; i > 0; i--) {
       generateId();
